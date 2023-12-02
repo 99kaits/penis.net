6cb5f1cd0b7ad579698f2552473f631e8c908ac1
99kaits <keightbillion@gmail.com>
2023-12-02 03:38:27 -0500

    hanging on in quiet desperation is the mono way

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # penis.net
 finally the iconic penis extender graces c#
 
-penis extender program on c#, project files are set up for .net core 8.0 but this will almost certainly build on even like ancient .net versions  
-literally just go into the folder and do `dotnet run` to run it, its that easy  
-then you can bask in the glory of the penis extension
+mono port  
+.net framework 4.7.2 port
--- penis.net/Program.cs
+++ penis.net/Program.cs
@@ -1,26 +1,28 @@
 ﻿using System;
-namespace penis.net;
-
-class Program
+namespace penis.net
 {
-    static void Main(string[] args)
+
+    class Program
     {
-        Console.Write("integer please: ");
-        string length = Console.ReadLine();
-        int lengthint;
-        bool isValid = int.TryParse(length, out lengthint);
-        if (isValid)
+        static void Main(string[] args)
         {
-            Console.Write('B');
-            for (int i = 0; i < lengthint; i++)
+            Console.Write("integer please: ");
+            string length = Console.ReadLine();
+            int lengthint;
+            bool isValid = int.TryParse(length, out lengthint);
+            if (isValid)
             {
-                Console.Write('=');
+                Console.Write('B');
+                for (int i = 0; i < lengthint; i++)
+                {
+                    Console.Write('=');
+                }
+                Console.WriteLine('D');
             }
-            Console.WriteLine('D');
-        }
-        else
-        {
-            Console.WriteLine("wow not an int");
+            else
+            {
+                Console.WriteLine("wow not an int");
+            }
         }
     }
 }

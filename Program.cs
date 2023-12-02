6844f7c87e9d64f15c5409c902930aeea6aaf209
99kaits <keightbillion@gmail.com>
2023-12-02 03:16:29 -0500

    the penis extender rises

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace penis.net;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("integer please: ");
+        string length = Console.ReadLine();
+        int lengthint;
+        bool isValid = int.TryParse(length, out lengthint);
+        if (isValid)
+        {
+            Console.Write('B');
+            for (int i = 0; i < lengthint; i++)
+            {
+                Console.Write('=');
+            }
+            Console.WriteLine('D');
+        }
+        else
+        {
+            Console.WriteLine("wow not an int");
+        }
+    }
+}

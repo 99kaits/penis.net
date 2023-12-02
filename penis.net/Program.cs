c3f1f735b6e981582bb9e2ad573e95618f466bc3
99kaits <keightbillion@gmail.com>
2023-12-02 14:52:47 -0500

    fixed up some things on the .net core side

--- penis.net/Program.cs
+++ penis.net/Program.cs
@@ -1,26 +1,25 @@
-﻿using System;
-namespace penis.net;
-
-class Program
+﻿namespace penis.net
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.Write("integer please: ");
-        string length = Console.ReadLine();
-        int lengthint;
-        bool isValid = int.TryParse(length, out lengthint);
-        if (isValid)
+        static void Main(string[] args)
         {
-            Console.Write('B');
-            for (int i = 0; i < lengthint; i++)
+            Console.Write("integer please: ");
+            string length = Console.ReadLine();
+            bool isValid = int.TryParse(length, out global::System.Int32 lengthint);
+            if (isValid)
             {
-                Console.Write('=');
+                Console.Write('B');
+                for (int i = 0; i < lengthint; i++)
+                {
+                    Console.Write('=');
+                }
+                Console.WriteLine('D');
             }
-            Console.WriteLine('D');
-        }
-        else
-        {
-            Console.WriteLine("wow not an int");
+            else
+            {
+                Console.WriteLine("wow not an int");
+            }
         }
     }
 }
